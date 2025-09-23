b5da8ac29c69b9765cf805eb2d0f384da87ead8f
Akshaya <132545615+AKSHAYAPALCHAMY-P@users.noreply.github.com>
2025-09-23 11:15:56 +0530

    ADDED: Generics

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace GenericsDemo
+{
+	class MyGenericClass<T>
+	{
+		private T GenericMemberVariable; //Fields
+
+		public MyGenericClass(T Value) //Constructors
+		{
+			GenericMemberVariable = Value;
+		}
+
+		public T GenericMethod(T GenericParameter) //Method
+		{
+			Console.WriteLine($"Parameter type: {typeof(T).ToString()}, Value: {GenericParameter}");
+			Console.WriteLine($"Return type: {typeof(T).ToString()}, Value: {GenericMemberVariable}");
+			return GenericMemberVariable;
+		}
+
+		public T GenericProperty { get; set; }
+	}
+
+	namespace GenericsDemo
+	{
+		class Program
+		{
+			static void Main()
+			{
+				//MyGenericClass<int> integerGenericClass = new MyGenericClass<int>(10);
+				//int val = integerGenericClass.GenericMethod(200);
+				//Console.WriteLine(val);
+				//Console.ReadKey();
+
+				MyGenericClass<string> stringGenericClass = new MyGenericClass<string>("Hello World");
+				stringGenericClass.GenericProperty = "This is a gemeric propertyexample";
+				string Result = stringGenericClass.GenericMethod("Generic Parameter");
+				Console.WriteLine(Result);
+				Console.ReadKey();
+			}
+		}
+	}
+}
---
+++ Generics/Generics_With_two_Parameters/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;using System.Runtime.InteropServices;
+
+namespace Generics;
+
+class SomeCLass()
+{
+	public void GenericMethod<T1, T2>(T1 Param1, T2 Param2)
+	{
+		Console.WriteLine($"Parameter t1 Type{typeof(T1)}:Parameter T2 type:{typeof(T2)}");
+		Console.WriteLine($"Parameter1 : {Param1} : Parameter 2 :{Param2}");
+	}
+}
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("generic method Example in c#");
+		SomeCLass s = new SomeCLass();
+		s.GenericMethod<int,int>(10,20);
+		s.GenericMethod<double,string>(10.5,"Hello");
+		s.GenericMethod<string,float>("Akshaya",20.5f);
+		Console.ReadLine();
+	}
+}
