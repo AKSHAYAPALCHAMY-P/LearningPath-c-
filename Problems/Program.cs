8a2e52390ad498cbda2443b5fcf9df3557740410
Akshaya <132545615+AKSHAYAPALCHAMY-P@users.noreply.github.com>
2025-10-04 14:02:20 +0530

    ADDED: Problem-2 (#2)
    Co-authored-by: Anu <anu@nyxalon.com>

---
+++ Problems/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FibonacciSeries
+{
+    class EvenfibonacciSeries
+    {
+        public static void Main(string[] args)
+        {
+            int nlimit = 4000000;
+            int n1 = 0, n2 = 1;
+            int evenSum = 0;
+
+            while(evenSum < nlimit)
+            {
+                int n3 = n1 + n2;
+                n1 = n2;
+                n2 = n3;
+                if(n3 % 2 == 0)
+                {
+                    evenSum += n3;
+                }
+
+                Console.WriteLine(evenSum);
+            }
+            
+        }
+    }
+}
