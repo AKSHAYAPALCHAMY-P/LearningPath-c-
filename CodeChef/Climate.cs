53beb9e32761123253e2572914d37d4d7a4665a4
Anu <anu@nyxalon.com>
2025-10-21 17:41:54 +0530

    ADDED: Problems

---
+++ CodeChef/Climate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Climate
+{
+    public static void Main()
+    {
+        string[] nClimate = Console.ReadLine().Split(' ');
+
+        int nRainy = int.Parse(nClimate[0]);
+        int nCloudy = int.Parse(nClimate[1]);
+
+        int nTotalDays = 7;
+
+        int nClear = nTotalDays - (nRainy + nCloudy);
+
+        Console.WriteLine(nClear);
+    }
+}
