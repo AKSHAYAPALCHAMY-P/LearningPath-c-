36827de9a852e6a3c4d839a7e80a319ed99cb50c
Akshaya <132545615+AKSHAYAPALCHAMY-P@users.noreply.github.com>
2025-10-21 17:45:51 +0530

    CodeChef (#4)
    * ADDED: Problem-2
    
    * ADDED: CodeChef Project Folder
    
    * ADDED: CodeChefProblems
    
    * ADDED: Problems
    
    * ADDED: Problems
    
    ---------
    
    Co-authored-by: Anu <anu@nyxalon.com>

---
+++ CodeChef/Double Rent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Test
+{
+    public static void Main()
+    {
+        int nRent = int.Parse(Console.ReadLine());
+
+        int nResult = 2 * nRent;
+
+        Console.WriteLine(nResult);
+    }
+}
