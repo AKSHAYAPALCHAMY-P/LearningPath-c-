b0ed89ee8310488bc58a4b01b9d54487044707bf
Akshaya <132545615+AKSHAYAPALCHAMY-P@users.noreply.github.com>
2025-10-24 18:12:28 +0530

    ADDED: Multiples of 3 and 5

---
+++ Multiples of 3 and 5/Largest_Palindrome_For_ThreeDigitNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices.JavaScript;
+
+namespace LargestPalindrome
+{
+	class LargestPalindromeOfThreeDigit
+	{
+		public static void Main(string[] args)
+		{
+			int K = 0;
+			for(int i = 100; i < 999; i++)
+			{
+				for(int j = i; j < 999; j++)
+				{
+					K = i * j;
+				}
+
+				Console.WriteLine(K);
+			}
+		}
+	}
+}
+
