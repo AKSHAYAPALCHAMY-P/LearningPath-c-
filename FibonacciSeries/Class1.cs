e21077faf58f585925dff798c913f2f2028c1dc7
Akshaya <132545615+AKSHAYAPALCHAMY-P@users.noreply.github.com>
2025-10-24 18:10:31 +0530

    ADDED: Fibonacci Series

---
+++ FibonacciSeries/Class1.cs
@@ -0,0 +1,22 @@
+﻿namespace FibonacciSeries
+{
+	public class FibonacciSeries
+	{
+		public static void main(string[] args)
+		{
+			int nValue = 1;
+			int nSum = 0;
+
+			if(nValue % 2 == 0)
+			{
+				if(nSum <= 4000000000)
+				{
+					nSum += nValue;
+				}
+			}
+
+			Console.WriteLine(nSum);
+		}
+
+	}
+}
