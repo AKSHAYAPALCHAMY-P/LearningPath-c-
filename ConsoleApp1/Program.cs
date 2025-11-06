397757561ad9165f89d88aaa119f23dbdbb8b8c0
Akshaya <132545615+AKSHAYAPALCHAMY-P@users.noreply.github.com>
2025-11-06 18:28:27 +0530

    ADDED: C# Problem solving

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿//namespace EasyPronounciation
+//{
+//	public class EasyPronounciationWord
+//	{
+//		#region Publics
+//		public static void Main(string[] args)
+//		{
+//			Console.WriteLine("Enter the testcases you want");
+//			bool bTestcase = int.TryParse(Console.ReadLine(), out int nTest);
+
+//			for(int i = 0; i < nTest; i++)
+//			{
+//				Console.WriteLine("Enter the input string");
+//				string strInputString = Console.ReadLine().ToLower();
+
+//				int nConsonantsCount = 0;
+
+//				bool IsHard = false;
+
+//				foreach(char c in strInputString)
+//				{
+//					if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+//					{
+//						nConsonantsCount = 0;
+//					}
+
+//					else
+//					{
+//						nConsonantsCount++;
+
+//						if(nConsonantsCount >= 4)
+//						{
+//							IsHard = true;
+//							break;
+//						}
+//					}
+//				}
+
+//				if(IsHard)
+//				{
+//					Console.WriteLine("NO");
+//				}
+//				else
+//				{
+//					Console.WriteLine("YES");
+//				}
+//			}
+//		}
+//		#endregion
+//	}
+//}
---
+++ HappyNewYear/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HappyNewYear
+{
+	public class NewYear
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Enter the time now");
+			DateTime dtInputTime = DateTime.Parse(Console.ReadLine());
+
+			DateTime nTotalHours = dtInputTime.Date.AddDays(1);
+
+			TimeSpan nRemainingHours = nTotalHours - dtInputTime;
+
+			Console.WriteLine(nRemainingHours);
+		}
+	}
+}
