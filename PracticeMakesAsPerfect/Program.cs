096087c3026ba7625f314df771a341c62e5515a9
Akshaya <132545615+AKSHAYAPALCHAMY-P@users.noreply.github.com>
2025-11-06 18:29:57 +0530

    ADDED:Practice makes as perfect problem

---
+++ PracticeMakesAsPerfect/Program.cs
@@ -0,0 +1,13 @@
+﻿//using System;
+
+//namespace Practice
+//{
+//	public class PracticeMakesPerfect
+//	{
+//		public static void Main(string[] args)
+//		{
+//			Console.Write("Enter the Input values");
+//			bool[] nArray = int.TryParse(Console.ReadLine(),out int[] nResult)
+//		}
+//	}
+//}
