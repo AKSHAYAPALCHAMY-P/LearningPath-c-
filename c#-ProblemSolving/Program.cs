0afe46cb4633c471f078f9bfcc98dcd86cf80d9d
Akshaya <132545615+AKSHAYAPALCHAMY-P@users.noreply.github.com>
2025-11-07 15:15:07 +0530

    ADDED: Play with String game

---
+++ c#-ProblemSolving/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace PlayingWithStrings
+{
+	public class PlayingWithStrings
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Enter the target string;");
+			string strTargetString = Console.ReadLine();
+
+			Console.WriteLine("Enter the Binary String:");
+			string strBinaryString = Console.ReadLine();
+
+			//char[] strBinaryStringArray = 
+
+			if(strBinaryString != strTargetString)
+			{
+				Console.WriteLine("No");
+			}
+
+			int nOnesCountInTargetString = strTargetString.Count(c => c == '0');
+			int nZeroesCountnTargetString = strTargetString.Count(c => c == '1');
+			int nOnesCountInBinaryString = strBinaryString.Count(c => c == '0');
+			int nZeroesCountInBinaryString = strBinaryString.Count(c => c == '1');
+
+			if(nOnesCountInBinaryString == nOnesCountInTargetString && nZeroesCountInBinaryString == nZeroesCountnTargetString)
+			{
+				Console.WriteLine("Yes");
+			}
+			else
+			{
+				//	for(int i = 0; i < strTargetString.Length; i++)
+				//	{
+				//		for(int j = 0; j < i; j++)
+				//		{
+				//		}
+				//	}
+				Console.WriteLine("No");
+			}
+		}
+	}
+}
+
